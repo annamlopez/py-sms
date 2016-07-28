d7f8f3de5b4e94f28bb1f8ab2f25b38c6a42cfeb
Anna Lopez <alopez@andover.edu>
2016-07-28 09:27:38 -0400

    message

--- handler.py
+++ handler.py
@@ -1,15 +1,24 @@
+from random import choice 
+
+print "Do you want to play a game of Would You Rather? Or talk to the Text Bot Shiloh? (type WYR or Shiloh, you can alternate between the two at any point.)"
+media_links_for_happy = ["https://www.buzzfeed.com/kaelintully/jurassic-park-is-a-gr8-film-raptor-shriek-all-the-damn-time?utm_term=.vnG4WN1zwV#.dj5XPlKv3w", "https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww4.pictures.zimbio.com%2Fmp%2FHuMZPUQrZeql.jpg&imgrefurl=http%3A%2F%2Fwww.zimbio.com%2FMovie%2BNews%2Farticles%2FvVqgDGD2AFT%2FEveryone%2BObsessed%2BInsanely%2BCute%2BBaby%2BDory&docid=kD-rxCfvcCdtMM&tbnid=g1G5p7E8gVILmM%3A&w=600&h=400&bih=630&biw=1280&ved=0ahUKEwjmrJOYh5XOAhUSBh4KHUNXCMcQMwhvKBkwGQ&iact=mrc&uact=8"]
+media_links_for_sad = ["https://www.google.com/imgres?imgurl=http%3A%2F%2Fmedia.brostrick.com%2Fwp-content%2Fuploads%2F2016%2F03%2F10075517%2Ffunny-donald-trump-president-memes-2016-hair.jpg&imgrefurl=http%3A%2F%2Fwww.brostrick.com%2Fviral%2Ffunny-political-memes-donald-trump-hillary-clinton-sanders-cruz%2F&docid=kyYVY3gYM-nMtM&tbnid=yVn63XKixEuEbM%3A&w=446&h=536&bih=630&biw=1280&ved=0ahUKEwjlkML7h5XOAhWLJB4KHeiYASEQMwheKAAwAA&iact=mrc&uact=8", ]
+
+
 def response_handler(body):
     message = ""
-    if body == 'start':
-        message = "You are in a boring class.  Do you 'take a nap' or 'take notes'?"
-    elif body == 'take a nap':
-        message = "You fall asleep forever...and ever...and ever.  Do you want to 'start' over?"
-    elif body == 'take notes':
-        message = "You furiously take notes as the teacher drones on.  Your friend taps you on the shoulder.  Do you 'turn around' or keep looking at your 'notes'"
-    elif body == 'turn around':
-        message = "You turn around, only to find that you have just stared into the eyes of a basilisk!  Oh well...do you want to 'start' over?"
-    elif body == 'notes':
-        message = "Unfortunately, this game is incomplete...please come back later for more!  Do you want to 'start' over?"
-    else:
-        message = "Invalid command.  Text 'start' to restart the game.  Or text 'pic please' for a random picture"
-    return message
+    if body == "WYR":
+        print "For next Would You Rather type in WYR."
+        message = choice(["Would you rather a. live one life that lasts 1,000 years or b. live 10 lives that last 100 years each?", "Would you rather have to a. sneeze but not be able to or b. have something stuck in your eye for an entire year?", "Would you rather a. wear a snow suit in the desert or b. be naked in Antarctica?", "Would you rather a. have no arms or b. no legs?", "Would you rather be filthy rich but suffer depression or be poor but happy?"])
+    elif body == "Shiloh":
+        message = "Hi! My name is Shiloh. How are you?? If you aren't feeling the bestest type :( for some amusing pictures! If you feel as if you are on top of the WOOOORLD type :) for some delightful pictures. If you're feeling just alright type :| for some pleasant pictures."
+    if body == ":(":
+        return media_links_for_sad
+    elif body == ":)":
+        message = choice(["https://www.buzzfeed.com/kaelintully/jurassic-park-is-a-gr8-film-raptor-shriek-all-the-damn-time?utm_term=.vnG4WN1zwV#.dj5XPlKv3w", "https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww4.pictures.zimbio.com%2Fmp%2FHuMZPUQrZeql.jpg&imgrefurl=http%3A%2F%2Fwww.zimbio.com%2FMovie%2BNews%2Farticles%2FvVqgDGD2AFT%2FEveryone%2BObsessed%2BInsanely%2BCute%2BBaby%2BDory&docid=kD-rxCfvcCdtMM&tbnid=g1G5p7E8gVILmM%3A&w=600&h=400&bih=630&biw=1280&ved=0ahUKEwjmrJOYh5XOAhUSBh4KHUNXCMcQMwhvKBkwGQ&iact=mrc&uact=8"])
+        
+    # else:
+    #     message = "Invalid command.  Text 'start' to restart the game.  Or text 'pic please' for a random picture"
+    return message 
+
+    
--- send_sms.py
+++ send_sms.py
@@ -9,8 +9,8 @@ from random import choice
 account_sid = "AC6f9854ebea5e96e6aeb74334fc887d98"
 auth_token = "49dd2ccc11461e2d5fd8d70354019417"  
 fromnumber = "+19782917255"
-tonumber = "+ put your number"
-body_text = "You are gorgeous gwow! What's your name ma?! I love you!"
+tonumber = "+19783141229"
+body_text = "'ey"
 ######################################################
 
 client = TwilioRestClient(account_sid, auth_token)
@@ -37,11 +37,15 @@ def send_media(fromnumber, tonumber, media_links):
 
 # send(fromnumber, tonumber, body_text)
 
-media_links = ["http://i.telegraph.co.uk/multimedia/archive/03597/POTD_chick_3597497k.jpg", 'https://upload.wikimedia.org/wikipedia/commons/5/55/Phillips_Academy_Andover_Coat_of_Arms.png', 'http://vignette1.wikia.nocookie.net/pokemon/images/f/fc/025Pikachu_OS_anime_5.png/revision/20150101093704', "http://all4desktop.com/data_images/original/4238212-pictures.jpg", "http://i.telegraph.co.uk/multimedia/archive/03519/potd-squirrel_3519920k.jpg", "http://i.telegraph.co.uk/multimedia/archive/03571/potd-squirrel_3571152k.jpg"]
-send_media(fromnumber, tonumber, choice(media_links))
+# media_links = ["http://i.telegraph.co.uk/multimedia/archive/03597/POTD_chick_3597497k.jpg", 'https://upload.wikimedia.org/wikipedia/commons/5/55/Phillips_Academy_Andover_Coat_of_Arms.png', 'http://vignette1.wikia.nocookie.net/pokemon/images/f/fc/025Pikachu_OS_anime_5.png/revision/20150101093704', "http://all4desktop.com/data_images/original/4238212-pictures.jpg", "http://i.telegraph.co.uk/multimedia/archive/03519/potd-squirrel_3519920k.jpg", "http://i.telegraph.co.uk/multimedia/archive/03571/potd-squirrel_3571152k.jpg"]
+# send_media(fromnumber, tonumber, choice(media_links))
 
-# user = raw_input("Enter a text to send your TWILIO number.  Or enter q to quit: \n> ")
-# while user != "q":
-# 	response = response_handler(user)
-# 	send(fromnumber, tonumber, response)
-# 	user = raw_input("Enter a text to send your TWILIO number.  Or enter q to quit: \n> ")
+# media_links_for_happy = ["https://www.buzzfeed.com/kaelintully/jurassic-park-is-a-gr8-film-raptor-shriek-all-the-damn-time?utm_term=.vnG4WN1zwV#.dj5XPlKv3w", "https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww4.pictures.zimbio.com%2Fmp%2FHuMZPUQrZeql.jpg&imgrefurl=http%3A%2F%2Fwww.zimbio.com%2FMovie%2BNews%2Farticles%2FvVqgDGD2AFT%2FEveryone%2BObsessed%2BInsanely%2BCute%2BBaby%2BDory&docid=kD-rxCfvcCdtMM&tbnid=g1G5p7E8gVILmM%3A&w=600&h=400&bih=630&biw=1280&ved=0ahUKEwjmrJOYh5XOAhUSBh4KHUNXCMcQMwhvKBkwGQ&iact=mrc&uact=8"]
+media_links_for_sad = ["https://www.google.com/imgres?imgurl=http%3A%2F%2Fmedia.brostrick.com%2Fwp-content%2Fuploads%2F2016%2F03%2F10075517%2Ffunny-donald-trump-president-memes-2016-hair.jpg&imgrefurl=http%3A%2F%2Fwww.brostrick.com%2Fviral%2Ffunny-political-memes-donald-trump-hillary-clinton-sanders-cruz%2F&docid=kyYVY3gYM-nMtM&tbnid=yVn63XKixEuEbM%3A&w=446&h=536&bih=630&biw=1280&ved=0ahUKEwjlkML7h5XOAhWLJB4KHeiYASEQMwheKAAwAA&iact=mrc&uact=8", ]
+
+
+user = raw_input("Enter a text to send your TWILIO number.  Or enter q to quit: \n> ")
+while user != "q":
+	response = response_handler(user)
+	send(fromnumber, tonumber, response)
+	user = raw_input("Enter a text to send your TWILIO number.  Or enter q to quit: \n> ")
