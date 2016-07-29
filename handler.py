51d625129458274561fd04f12433d3d79bb82d4d
Anna Lopez <alopez@andover.edu>
2016-07-29 08:47:28 -0400

    blah

--- handler.py
+++ handler.py
@@ -8,11 +8,11 @@ media_links_for_alright = ["http://www.boredpanda.com/cute-bunnies/", "data:imag
 
 def response_handler(body):
     message = ""
-    if body == "WYR":
+    if body == "wyr":
         message = "For next Would You Rather type in WYR."
         # print "For next Would You Rather type in WYR."
         message = choice(["Would you rather a. live one life that lasts 1,000 years or b. live 10 lives that last 100 years each?", "Would you rather have to a. sneeze but not be able to or b. have something stuck in your eye for an entire year?", "Would you rather a. wear a snow suit in the desert or b. be naked in Antarctica?", "Would you rather a. have no arms or b. no legs?", "Would you rather be filthy rich but suffer depression or be poor but happy?"])
-    elif body == "Shiloh":
+    elif body == "shiloh":
         message = "Hi! My name is Shiloh. How are you?? If you aren't feeling the bestest type :( for some amusing pictures! If you feel as if you are on top of the WOOOORLD type :) for some delightful pictures. If you're feeling just alright type :| for some pleasant pictures."
     if body == ":(":
         return choice(["https://www.google.com/imgres?imgurl=http%3A%2F%2Fmedia.brostrick.com%2Fwp-content%2Fuploads%2F2016%2F03%2F10075517%2Ffunny-donald-trump-president-memes-2016-hair.jpg&imgrefurl=http%3A%2F%2Fwww.brostrick.com%2Fviral%2Ffunny-political-memes-donald-trump-hillary-clinton-sanders-cruz%2F&docid=kyYVY3gYM-nMtM&tbnid=yVn63XKixEuEbM%3A&w=446&h=536&bih=630&biw=1280&ved=0ahUKEwjlkML7h5XOAhWLJB4KHeiYASEQMwheKAAwAA&iact=mrc&uact=8", "https://www.youtube.com/watch?v=kNKu1uNBVkU"])
